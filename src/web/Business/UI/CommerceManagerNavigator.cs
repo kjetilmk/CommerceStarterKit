6ca7b344e2a2852511b4d2b70f5385c7b4fa03ee
evest <steve@celius.cc>
2015-03-17 12:37:45 +0100

    New page type and misc improvements
    Added quick link to Commerce Manager, better styling for TinyMCE,
    improved logo and header on mobile (low resolution tweaks), added new
    page type for articles with sidebar content area. Fixed styling for
    content areas (overflow hidden)

---
+++ src/web/Business/UI/CommerceManagerNavigator.cs
@@ -0,0 +1,40 @@
+﻿/*
+Commerce Starter Kit for EPiServer
+
+All rights reserved. See LICENSE.txt in project root.
+
+Copyright (C) 2013-2015 Oxx AS
+Copyright (C) 2013-2015 BV Network AS
+
+*/
+using System.Collections.Generic;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.PageExtensions;
+
+namespace OxxCommerceStarterKit.Web.Business.UI
+{
+    [ServiceConfiguration(ServiceType = typeof(IQuickNavigatorItemProvider))]
+    public class CommerceManagerQuickNavigator : IQuickNavigatorItemProvider
+    {
+        private int _sortOrder = 50;
+
+        public IDictionary<string, QuickNavigatorMenuItem> GetMenuItems(ContentReference currentContent)
+        {
+            string commerceManagerLink = System.Configuration.ConfigurationManager.AppSettings["CommerceManagerLink"];
+            if (commerceManagerLink == null)
+                return null;
+
+            var menuItems = new Dictionary<string, QuickNavigatorMenuItem>();
+            menuItems.Add("qn-commerce-manager", new QuickNavigatorMenuItem("Commerce Manager", commerceManagerLink, null, null, null));
+            return menuItems;
+        }
+
+        public int SortOrder
+        {
+            get { return _sortOrder; }
+            private set { _sortOrder = value; }
+        }
+    }
+}
---
+++ src/web/Controllers/ArticleWithSidebarPageController.cs
@@ -0,0 +1,47 @@
+﻿/*
+Commerce Starter Kit for EPiServer
+
+All rights reserved. See LICENSE.txt in project root.
+
+Copyright (C) 2013-2014 Oxx AS
+Copyright (C) 2013-2014 BV Network AS
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Filters;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.GoogleAnalytics.Helpers;
+using EPiServer.Security;
+using EPiServer.Web.Mvc;
+using Mediachase.Commerce.Customers;
+using OxxCommerceStarterKit.Web.Business.Analytics;
+using OxxCommerceStarterKit.Web.Models.PageTypes;
+using OxxCommerceStarterKit.Web.Models.ViewModels;
+
+namespace OxxCommerceStarterKit.Web.Controllers
+{
+    [TemplateDescriptor()]
+    public class ArticleWithSidebarPageController : PageControllerBase<ArticleWithSidebarPage>
+    {
+		private readonly IContentLoader _contentLoader;
+
+        public ArticleWithSidebarPageController(IContentLoader contentLoader)
+        {            
+			_contentLoader = contentLoader;		    
+        }
+
+        public ViewResult Index(PageData currentPage)
+        {
+            var viewPath = GetViewForPageType(currentPage);
+
+            var model = CreatePageViewModel(currentPage);
+
+            return View(viewPath, model);
+        }
+    }
+}
